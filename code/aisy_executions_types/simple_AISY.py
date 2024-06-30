bf75546cacb31d507e1a758a0d8d7b1a4b0cbe9b
bermejo4 <minijai72@gmail.com>
2024-06-30 13:25:03 +0200

    added code to show error in terminal

--- code/aisy_executions_types/simple_AISY.py
+++ code/aisy_executions_types/simple_AISY.py
@@ -59,6 +59,8 @@ for i in range(0, max_dataset_num):
                     aisy.run()
                 except Exception as e:
                     telegram_notification(f"Error in nn: {str(nn.__name__)} batch: {batchsize} and byte: {byte}")
+                    print("------> ERROR: ", str(e))
+
 
 telegram_notification("The program has finished")
 
